702a2919ec442fe7eb74cfaf8789bdeeda796c85
Janus <ysangkok@gmail.com>
2018-03-15 17:38:02 +0100

    lightning: complete moving of lightning objects, acquire net/wallet lock while answering lightning requests

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -60,6 +60,7 @@ from .transaction_dialog import show_transaction
 from .fee_slider import FeeSlider
 from .util import *
 from .installwizard import WIF_HELP_TEXT
+from .lightning_invoice_list import LightningInvoiceList
 
 
 class StatusBarButton(QPushButton):
@@ -140,6 +141,8 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         tabs.addTab(self.create_history_tab(), QIcon(":icons/tab_history.png"), _('History'))
         tabs.addTab(self.send_tab, QIcon(":icons/tab_send.png"), _('Send'))
         tabs.addTab(self.receive_tab, QIcon(":icons/tab_receive.png"), _('Receive'))
+        self.lightning_invoices_tab = self.create_lightning_invoices_tab(wallet)
+        tabs.addTab(self.lightning_invoices_tab, _("Lightning Invoices"))
 
         def add_optional_tab(tabs, tab, icon, description, name):
             tab.tab_icon = icon
@@ -763,6 +766,10 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         self.contact_list.update()
         self.invoice_list.update()
         self.update_completions()
+
+    def create_lightning_invoices_tab(self, wallet):
+        self.lightning_invoice_list = LightningInvoiceList(self, wallet.network.lightningworker, wallet.network.lightningrpc)
+        return self.lightning_invoice_list
 
     def create_history_tab(self):
         from .history_list import HistoryList
@@ -1914,6 +1921,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         console.updateNamespace({'wallet' : self.wallet,
                                  'network' : self.network,
                                  'plugins' : self.gui_object.plugins,
+                                 'lightning' : self.gui_object.lightning,
                                  'window': self})
         console.updateNamespace({'util' : util, 'bitcoin':bitcoin})
 
